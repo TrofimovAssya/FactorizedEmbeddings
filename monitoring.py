8fcad30a533756e43f4b90582461f24c83b53871
TrofimovAssya <petitkalimero@gmail.com>
2019-07-16 14:16:56 -0400

    changed the monitoring to remove R plot functions

--- monitoring.py
+++ monitoring.py
@@ -6,69 +6,6 @@ import shutil
 import models
 import datetime
 
-def save_everything(dir_name, epoch, model, dataset):
-
-    #import ipdb; ipdb.set_trace()
-
-    if not os.path.exists(dir_name):
-        os.makedirs(dir_name)
-
-    file_name = os.path.join(dir_name, 'gene_epoch_{}'.format(epoch))
-
-    emb = model.emb_1.weight.cpu().data.numpy()
-    np.save(file_name,emb)
-
-    emb = model.emb_2.weight.cpu().data.numpy()
-    np.save(file_name,emb)
-    file_name = os.path.join(dir_name, 'patient_epoch_{}'.format(epoch))
-    np.save(file_name, emb)
-    #dump_emb_size2(emb, file_name, dataset.extra_info(), dataset.nb_patient)
-
-
-def save_predictions(dir_name, predictions):
-    file_name = os.path.join(dir_name, 'predictions')
-    np.save(file_name, predictions)
-
-
-def dump_emb_size2(emb, file_name, extra_info, nb_patient):
-
-    assert emb.shape[1] == 2
-
-    #b = open(file_name, 'wb')
-    np.save(file_name,emb)
-
-    # info_keys, info_values = extra_info.keys(), extra_info.values()
-    # import pdb; pdb.set_trace()
-    # b.write("\t".join(['dimension1', 'dimension2'] + info_keys) + '\n')
-
-    # for p in xrange(nb_patient):
-    #     b.write("\t".join([str(emb[p, 0]), str(emb[p, 1])] + [str(info[p]) for info in info_values]) + '\n')
-    # b.close()
-
-def dump_error_by_gene(pred, data, file_name, dir_name):
-    if not os.path.exists(dir_name):
-                os.makedirs(dir_name)
-    assert pred.shape == data.shape
-    a = (data-pred)**2
-    meanval = np.mean(a,axis=1)
-    np.save(file_name,meanval)
-
-def dump_error_by_tissue(pred, data, file_name, dir_name, data_type, nb_patient):
-
-
-    #import ipdb; ipdb.set_trace()
-
-    if not os.path.exists(dir_name):
-        os.makedirs(dir_name)
-    assert pred.shape == data.shape
-    b = open(file_name, 'wb')
-    b.write("\t".join(['type','MSE']) + '\n')
-    for ttypes in set(data_type):
-        meanval = np.mean((pred[:,data_type == ttypes] - data[:,data_type == ttypes])**2)
-        b.write("\t".join([ttypes, str(meanval)]) + '\n')
-    b.close()    
-
-    
 def create_experiment_folder(opt):
 
     params = vars(opt).copy()
@@ -158,18 +95,3 @@ def load_checkpoint(load_folder, opt, input_size, filename='checkpoint.pth.tar')
     print (my_model)
 
     return my_model, optimizer, epoch, new_opt
-
-#
-# def sample_embedding_dump(emb, epoch, g_emb_size, data_type, data_subtype, pca=False, nmf=False):
-#     if pca:
-#         b = open('pca_embeddings', 'wb')
-#     elif nmf:
-#         b = open('nmf_emebddings', 'wb')
-#     else:
-#         b = open('_'.join(['embeddings', str(epoch), str(g_emb_size)]), 'wb')
-#         emb = emb[0]
-#
-#     b.write("\t".join(['dimension1', 'dimension2', 'type', 'subtype']) + '\n')
-#     for p in xrange(nb_patient):
-#         b.write("\t".join([str(emb[p, 0]), str(emb[p, 1]), str(data_type[p]), str(data_subtype[p])]) + '\n')
-#     b.close()
